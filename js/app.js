29f29abfe2dee6d76f5f8dc5931d5673f3bfc946
qoo41016 <102722787+qoo41016@users.noreply.github.com>
2022-04-25 14:35:18 +0800

    2022/04/25

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/0.5.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/0.4.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
